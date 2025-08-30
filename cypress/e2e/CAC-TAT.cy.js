9d660c64c8a6c4858f951f76ad878f04c0ebf516
Raphael Borba <raphaelborba10@gmail.com>
2025-08-30 09:55:35 -0300

    Aula 1 - Usando cy.visit, cy.title e should

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+describe("Central de Atendimento ao Cliente TAT", () => {
+  it("verifica o título da aplicação", () => {
+    cy.visit("./src/index.html");
+    cy.title().should("eq", "Central de Atendimento ao Cliente TAT");
+  });
+});
